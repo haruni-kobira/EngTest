42c78f630ba54612685d429aa246c4307d35948d
KobiraKyohei <haruni.kobira@gmail.com>
2024-05-08 13:40:31 +0900

    DB接続 コメントアウト

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const app = express();
-const Redis = require('ioredis');
-const cfenv = require('cfenv');
-
+// const Redis = require('ioredis');
 
 // Redis
-const client = new Redis({
-  host: 'nakamine-cache.gpddhc.clustercfg.apse2.cache.amazonaws.com', // rename the credentials `hostname` to `host`
-  port: 6379,
-  password: null,
-});
-
-
+// const client = new Redis({
+//   host: 'nakamine-cache.gpddhc.clustercfg.apse2.cache.amazonaws.com', // rename the credentials `hostname` to `host`
+//   port: 6379,
+//   password: null,
+// });
 
 
 // 接続先
@@ -19,17 +15,15 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/add', async (req, res) => {
-    res.send('Add');
-    await client.set('Book', 'BooBook');
-    // const kitty = new Cat({ name: 'Ta' });
-    // kitty.save().then(() => console.log('meow'));
-});
+// app.get('/add', async (req, res) => {
+//     res.send('Add');
+//     await client.set('Book', 'BooBook');
+// });
 
-app.get('/find', async (req, res) => {
-    var response = await client.get('Book');
-    res.send(response);
-});
+// app.get('/find', async (req, res) => {
+//     var response = await client.get('Book');
+//     res.send(response);
+// });
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
